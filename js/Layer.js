db315f4e1c3e9d34650cfb8c976ba10373012b21
Lena Partyka <partyka.lena.f@gmail.com>
2023-02-22 21:39:52 +0300

    create SPA; stlit into modules script.js

---
+++ js/Layer.js
@@ -0,0 +1,20 @@
+//слои паралакса
+class Layer {
+  constructor(game, image, speedModifier) {
+    this.game = game;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.width = 1283;
+    this.heigth = 770;
+    this.x = 0;
+    this.y = 0;
+  }
+  update() {
+    if (this.x <= -this.width) this.x = 0;
+    this.x -= this.game.speed * this.speedModifier;
+  }
+  draw(context) {
+    context.drawImage(this.image, this.x, this.y);
+    context.drawImage(this.image, this.x + this.width, this.y);
+  }
+}
---
+++ js/Particle.js
@@ -0,0 +1,39 @@
+// шестеренки, оставшиеся от врагов
+class Particle {
+  constructor(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.image = document.getElementById('gears');
+    this.frameX = Math.floor(Math.random() * 3);
+    this.frameY = Math.floor(Math.random() * 3);
+    this.spriteSize = 70;
+    this.sizeModifier = (Math.random() * 0.5 + 0.5).toFixed(1);
+    this.size = this.spriteSize * this.sizeModifier;
+    this.speedX = Math.random() * 6 - 3;
+    this.speedY = Math.random() * -15;
+    this.gravity = 0.3;
+    this.markedForDeletion = false;
+    this.angle = 0;
+    this.va = Math.random() * 0.2 - 0.1;
+    this.bounced = false;
+    this.bottomBounceBoundary = Math.random() * 130 + 90;
+  }
+
+  update() {
+    this.angle += this.va;
+    this.speedY += this.gravity;
+    this.x -= this.speedX + this.game.speed;
+    this.y += this.speedY;
+    if (this.y > this.game.height + this.size || this.x < 0 - this.size) this.markedForDeletion = true;
+    if (this.y > this.game.height - this.bottomBounceBoundary && !this.bounced) {
+      this.bounced = true;
+      this.speedY *= -0.9;
+    }
+  }
+
+  draw(context) {
+    context.drawImage(this.image, this.frameX * this.spriteSize, this.frameY * this.spriteSize, this.spriteSize,
+      this.spriteSize, this.x, this.y, this.size, this.size);
+  }
+}
